8dd2393a55961fbcdcbb96ee6ff68e907a130a46
John O'Sullivan <josullivan1@luc.edu>
2016-08-02 11:09:59 -0500

    Init Push
    Init Push

---
+++ SwiftCommon/SwiftCommon/SwiftCommon.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCommon.h
+//  SwiftCommon
+//
+//  Created by John O'Sullivan on 8/2/16.
+//  Copyright © 2016 John O'Sullivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCommon.
+FOUNDATION_EXPORT double SwiftCommonVersionNumber;
+
+//! Project version string for SwiftCommon.
+FOUNDATION_EXPORT const unsigned char SwiftCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCommon/PublicHeader.h>
+
+
